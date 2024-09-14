4591253bc0603d347ef870f7a3ecbfb6e0a2c01a
Maykon Oliveira <maykonoliveira850@gmail.com>
2024-09-14 11:34:39 -0300

    feat: postgresql configuration

--- drizzle/index.ts
+++ drizzle/index.ts
@@ -1,14 +1,13 @@
-import { DrizzleSQLiteAdapter } from "@lucia-auth/adapter-drizzle";
-import { createClient } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 import { sessions, todos, users } from "./schema";
 
-// Setup sqlite database connection
-const client = createClient({
-  url: process.env.DATABASE_URL ?? "file:sqlite.db",
-  authToken: process.env.DATABASE_AUTH_TOKEN,
+const client = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
+
 export const db = drizzle(client, { schema: { users, sessions, todos } });
 
 // Setup lucia adapter
-export const luciaAdapter = new DrizzleSQLiteAdapter(db, sessions, users);
+export const luciaAdapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
